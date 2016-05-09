c056b88ee8ea2e73cd7222ea9f389dda98d5dd81
haohan <haohan@huoyunren.com>
2016-05-10 01:42:36 +0800

    新增管理员首页

--- www/load/checkLogin.js
+++ www/load/checkLogin.js
@@ -1,3 +1,4 @@
+//检查登录状态，确定top和left的内容
 $.ajax({
     type: 'GET',
     crossDomain: true,
@@ -37,7 +38,20 @@ $.ajax({
             });
         }
         if (result.result == 2) {//管理员已登录
-
+            $.ajax({//插入top
+                url: "top_admin.html",
+                cache: false,
+                success: function (html) {
+                    $("#top").replaceWith(html);
+                }
+            });
+            $.ajax({//插入left
+                url: "left_admin.html",
+                cache: false,
+                success: function (html) {
+                    $("#left").replaceWith(html);
+                }
+            });
         }
     },
     error: function () {
