68318b927dedaeb8ae586d80fa1f8d28501bb411
haohan <haohan@huoyunren.com>
2016-05-11 16:13:00 +0800

    新增添加商品

--- Application/Home/Controller/ManageProductController.class.php
+++ Application/Home/Controller/ManageProductController.class.php
@@ -48,6 +48,24 @@ class ManageProductController extends Controller
     public function createProduct()
     {
         $params = I('post.');
+        //拿上传的封面
+        $upload = new \Think\Upload();
+        $upload->exts = array('jpg', 'gif', 'png', 'jpeg');
+        $upload->rootPath = 'Uploads/'; // 设置附件上传根目录
+        $upload->savePath = 'cover/'; // 设置附件上传（子）目录
+        $upload->saveName = md5(uniqid());
+        //get extention of the file:
+        $nameReverse = strrev($_FILES["cover"]['name']); //reverse the file name
+        $cutString = explode(".", $nameReverse);
+        $upload->saveExt = strrev($cutString[0]);
+        $upload->autoSub = false;
+        // 上传文件
+        $info = $upload->upload();
+        if (!$info) {// 上传错误提示错误信息
+            //$this->error($upload->getError());
+        } else {// 上传成功
+            $params['cover'] = C('webroot') . $upload->rootPath . $upload->savePath . $upload->saveName . '.' . $upload->saveExt;
+        }
         $result = D('ManageProduct')->createProduct($params);
         echo json_encode($result);
     }
@@ -99,7 +117,7 @@ class ManageProductController extends Controller
         $result = D('ManageProduct')->deleteProduct($params);
         echo json_encode($result);
     }
-    
+
     /**
      * 编辑商品，通过id获取已有的商品信息
      * 
@@ -111,7 +129,7 @@ class ManageProductController extends Controller
         $result = D('ManageProduct')->editProduct($params);
         echo json_encode($result);
     }
-    
+
     /**
      * 保存编辑信息
      * 
@@ -123,4 +141,5 @@ class ManageProductController extends Controller
         $result = D('ManageProduct')->saveEdit($params);
         echo json_encode($result);
     }
+
 }
